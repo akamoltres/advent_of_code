d295498f152ff29e599c167742ec6f9322b4500d
Abhi K <akamoltres@gmail.com>
2020-11-29 22:42:59 -0800

    2019/2 part 2 complete

--- 2019/2.c
+++ 2019/2.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 
 // Returns length of intcode program if success
 // Returns -1 if file is invalid
@@ -94,7 +95,37 @@ int part1()
     return buffer[0];
 }
 
+// Returns -1 if failed
+int part2()
+{
+    const int bufsize = 200;
+    int buffer[bufsize];
+
+    int program_length = read_intcode(bufsize, buffer, "2.txt");
+
+    const int desired_output = 19690720;
+
+    for(int noun = 0; noun < 100; ++noun)
+    {
+        for(int verb = 0; verb < 100; ++verb)
+        {
+            int test_program[bufsize];
+            memcpy(test_program, buffer, sizeof(int) * bufsize);
+            test_program[1] = noun;
+            test_program[2] = verb;
+            (void) run_intcode(program_length, test_program);
+            if(test_program[0] == desired_output)
+            {
+                return noun * 100 + verb;
+            }
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     printf("Part 1: %d\n", part1());
+    printf("Part 2: %d\n", part2());
 }
