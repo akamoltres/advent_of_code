eb9fd944d77e19fd99805372068b07f5adc09b11
Abhi K <akamoltres@gmail.com>
2020-12-07 22:27:39 -0800

    2020/8 complete

--- 2020/8.c
+++ 2020/8.c
@@ -29,10 +29,13 @@ int get_input(const int bufsize, Command_t *buffer)
         fscanf(fp, "%d", &buffer[num_commands++].arg);
         if(num_commands == bufsize)
         {
+            fclose(fp);
             num_commands = -1;
             return num_commands;
         }
     }
+
+    fclose(fp);
 
     return num_commands;
 }
@@ -80,7 +83,77 @@ int part1()
     return acc;
 }
 
+// Returns -1 if failed unexpectedly
+int part2()
+{
+    const int bufsize = 5000;
+    Command_t buffer[bufsize];
+
+    int num_commands = get_input(bufsize, buffer);
+    if(num_commands == -1)
+    {
+        return -1;
+    }
+
+    for(int i = 0; i < num_commands; ++i)
+    {
+        int pc = 0;
+        int acc = 0;
+
+        Command_t program[bufsize];
+        memcpy(program, buffer, num_commands * sizeof(Command_t));
+
+        int call_count[bufsize];
+        memset(call_count, 0, bufsize * sizeof(int));
+
+        while(!call_count[pc])
+        {
+            call_count[pc] += 1;
+
+            if(!strcmp("nop", buffer[pc].op))
+            {
+                if(pc != i)
+                {
+                    pc += 1;
+                }
+                else
+                {
+                    pc += buffer[pc].arg;
+                }
+            }
+            else if(!strcmp("jmp", buffer[pc].op))
+            {
+                if(pc != i)
+                {
+                    pc += buffer[pc].arg;
+                }
+                else
+                {
+                    pc += 1;
+                }
+            }
+            else if(!strcmp("acc", buffer[pc].op))
+            {
+                acc += buffer[pc].arg;
+                pc += 1;
+            }
+            else
+            {
+                return -1;
+            }
+
+            if(pc == num_commands)
+            {
+                return acc;
+            }
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     printf("Part 1: %d\n", part1());
+    printf("Part 2: %d\n", part2());
 }
