9ec5c29586b6e8d38aae6c5a62b8a6dd9da21843
Abhi K <akamoltres@gmail.com>
2020-12-09 00:54:52 -0800

    2020/9 complete

--- 2020/9.c
+++ 2020/9.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int read_input(char *input_filename, const int bufsize, int *buffer)
+int read_input(char *input_filename, const int bufsize, long *buffer)
 {
     int list_length = 0;
 
@@ -12,7 +12,7 @@ int read_input(char *input_filename, const int bufsize, int *buffer)
         return -1;
     }
 
-    while(fscanf(fp, "%d", buffer + list_length) != EOF)
+    while(fscanf(fp, "%ld", buffer + list_length) != EOF)
     {
         list_length += 1;
     }
@@ -22,7 +22,7 @@ int read_input(char *input_filename, const int bufsize, int *buffer)
     return list_length;
 }
 
-static int is_sum(int num, int *buffer, int preamble_start, int preamble_end)
+static int is_sum(long num, long *buffer, int preamble_start, int preamble_end)
 {
     for(int i = preamble_start; i < preamble_end; ++i)
     {
@@ -38,10 +38,12 @@ static int is_sum(int num, int *buffer, int preamble_start, int preamble_end)
     return 0;
 }
 
-int part1(char *input_filename, int preamble_length)
+// If retval == 0: returns the value of the first failing number
+// If retval == 1: returns the index of the first failing number
+long part1(char *input_filename, int preamble_length, int retval)
 {
     const int bufsize = 2000;
-    int buffer[bufsize];
+    long buffer[bufsize];
 
     int list_length = read_input(input_filename, bufsize, buffer);
     if(list_length == -1)
@@ -53,15 +55,63 @@ int part1(char *input_filename, int preamble_length)
     {
         if(!is_sum(buffer[i], buffer, i - preamble_length, i))
         {
-            return buffer[i];
+            if(retval == 0)
+            {
+                return buffer[i];
+            }
+            else if(retval == 1)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 
     return -1;
 }
 
-int part2(char *input_filename)
+long part2(char *input_filename, int preamble_length)
 {
+    const int bufsize = 2000;
+    long buffer[bufsize];
+    long sums[bufsize];
+
+    int list_length = read_input(input_filename, bufsize, buffer);
+    if(list_length == -1)
+    {
+        return -1;
+    }
+
+    long invalid_index = part1(input_filename, preamble_length, 1);
+
+    sums[0] = buffer[0];
+    for(int i = 1; i < list_length; ++i)
+    {
+        sums[i] = sums[i - 1] + buffer[i];
+    }
+
+    for(int i = 0; i < invalid_index; ++i)
+    {
+        for(int j = i + 1; j < invalid_index; ++j)
+        {
+            if(sums[j] - sums[i] == buffer[invalid_index])
+            {
+                long min = buffer[i];
+                long max = buffer[i];
+                for(int k = i + 1; k < j; ++k)
+                {
+                    min = (min < buffer[k] ? min : buffer[k]);
+                    max = (max > buffer[k] ? max : buffer[k]);
+                }
+
+                return (min + max);
+            }
+        }
+    }
+
     return -1;
 }
 
@@ -73,8 +123,8 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    printf("Part 1: %d\n", part1(argv[1], atoi(argv[2])));
-    printf("Part 2: %d\n", part2(argv[1]));
+    printf("Part 1: %ld\n", part1(argv[1], atoi(argv[2]), 0));
+    printf("Part 2: %ld\n", part2(argv[1], atoi(argv[2])));
 
     return 0;
 }
