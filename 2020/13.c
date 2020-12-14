adbc92585898021bd5ade2cae1c57598fcc5125c
Abhi K <akamoltres@gmail.com>
2020-12-13 22:03:49 -0800

    2020/13 complete

--- 2020/13.c
+++ 2020/13.c
@@ -108,86 +108,44 @@ ull part2(char *input_filename)
 {
     const int bufsize = 1000;
     int buffer[bufsize];
-    int departure;
+    int throwaway;
 
-    int num_buses = get_input(input_filename, bufsize, buffer, &departure);
+    int num_buses = get_input(input_filename, bufsize, buffer, &throwaway);
     if(num_buses == -1)
     {
         return -1;
     }
 
-    int max_bus_idx = -1;
+    ull departure = 0;
+    ull step = 1;
     for(int i = 0; i < num_buses; ++i)
     {
         if(buffer[i] != -1)
         {
-            if(buffer[max_bus_idx] < buffer[i] || max_bus_idx == -1)
+            for(; ; departure += step)
             {
-                max_bus_idx = i;
-            }
-        }
-    }
-
-    /*
-    int start_idx = -1;
-    while(buffer[++start_idx] == -1);
-    ull lcm_total = buffer[start_idx];
-    for(int i = start_idx + 1; i < num_buses; ++i)
-    {
-        if(buffer[i] != -1)
-        {
-            lcm_total = lcm(lcm_total, buffer[i]);
-        }
-    }
-    printf("%llu\n", lcm_total);
-    return -1;
-    */
-
-    // ull start_ts = 100000000000000 + (buffer[max_bus_idx] - (100000000000000 % buffer[max_bus_idx]));
-    // ull start_ts = buffer[max_bus_idx];
-    // ull start_ts = 1658064091492211;
-    ull start_ts = 1;
-    for(int i = 0; i < num_buses; ++i)
-    {
-        if(buffer[i] != -1)
-        {
-            start_ts *= buffer[i];
-        }
-    }
-    printf("start: %llu\n", start_ts);
-
-    for(ull timestamp = start_ts; timestamp > buffer[max_bus_idx]; timestamp -= buffer[max_bus_idx])
-    {
-        int fail = 0;
-        for(int i = 0; i < num_buses; ++i)
-        {
-            if(buffer[i] != -1)
-            {
-                if(i < max_bus_idx)
+                int good = 1;
+                for(int j = 0; j <= i; ++j)
                 {
-                    if(max_bus_idx - i != timestamp % buffer[i])
+                    if(buffer[j] != -1)
                     {
-                        fail = 1;
-                        break;
+                        if((departure + j) % buffer[j] != 0)
+                        {
+                            good = 0;
+                            break;
+                        }
                     }
                 }
-                else if(max_bus_idx < i)
+                if(good)
                 {
-                    if(i - max_bus_idx != buffer[i] - (timestamp % buffer[i]))
-                    {
-                        fail = 1;
-                        break;
-                    }
+                    break;
                 }
             }
-        }
-        if(!fail)
-        {
-            printf("%llu\n", timestamp - max_bus_idx);
+            step *= buffer[i];
         }
     }
 
-    return -1;
+    return departure;
 }
 
 int main(int argc, char *argv[])
