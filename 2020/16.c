9b96c6ac8792957a649da3350a5c14f8822e52c3
Abhi K <akamoltres@gmail.com>
2020-12-16 02:09:33 -0800

    2020/16 complete

--- 2020/16.c
+++ 2020/16.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 
 #define FIELD_LENGTH 100
 #define MAX_NUM_TICKETS 1000
@@ -182,9 +183,133 @@ int part1(char *input_filename)
     return error_rate;
 }
 
-int part2(char *input_filename)
+unsigned long part2(char *input_filename)
 {
-    return -1;
+    int num_fields;
+    char field_names[MAX_NUM_FIELDS][FIELD_LENGTH];
+    int fields[MAX_NUM_FIELDS][4];
+
+    int num_tickets;
+    int tickets[MAX_NUM_TICKETS][MAX_NUM_FIELDS];
+
+    int retval = get_input(input_filename, &num_fields, field_names, fields, &num_tickets, tickets);
+    if(retval != 0)
+    {
+        return retval;
+    }
+
+    int field_assigned[MAX_NUM_FIELDS];
+    memset(field_assigned, 0, MAX_NUM_FIELDS * sizeof(int));
+
+    int field_used[MAX_NUM_FIELDS];
+    memset(field_used, 0, MAX_NUM_FIELDS * sizeof(int));
+
+    int ticket_valid[MAX_NUM_TICKETS];
+    int error_rate = 0;
+
+    for(int i = 1; i < num_tickets; ++i)
+    {
+        int ticket_invalid = 0;
+        for(int k = 0; k < num_fields; ++k)
+        {
+            int invalid = 1;
+            for(int j = 0; j < num_fields; ++j)
+            {
+                if((fields[j][0] <= tickets[i][k] && tickets[i][k] <= fields[j][1]) ||
+                   (fields[j][2] <= tickets[i][k] && tickets[i][k] <= fields[j][3]))
+                {
+                    invalid = 0;
+                    break;
+                }
+            }
+            if(invalid)
+            {
+                error_rate += tickets[i][k];
+                ticket_invalid = 1;
+            }
+        }
+        ticket_valid[i] = !ticket_invalid;
+    }
+
+    // is field [subscript0] valid for field [subscript1] on the tickets?
+    int is_candidate[MAX_NUM_FIELDS][MAX_NUM_FIELDS];
+    memset(is_candidate, 0, MAX_NUM_FIELDS * MAX_NUM_FIELDS * sizeof(int));
+    for(int i = 0; i < num_fields; ++i) // is field i...
+    {
+        for(int j = 0; j < num_fields; ++j) // ...valid for the j'th ticket field?
+        {
+            is_candidate[i][j] = 1;
+            for(int k = 1; k < num_tickets; ++k)
+            {
+                if(ticket_valid[k])
+                {
+                    if(!(fields[i][0] <= tickets[k][j] && tickets[k][j] <= fields[i][1]) &&
+                       !(fields[i][2] <= tickets[k][j] && tickets[k][j] <= fields[i][3]))
+                    {
+                        is_candidate[i][j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    int remaining_candidates = num_fields;
+    while(remaining_candidates > 0)
+    {
+        // check if there are any ticket fields with just one candidate
+        for(int i = 0; i < num_fields; ++i)
+        {
+            // don't stomp on ticket fields that are taken care of
+            if(!field_used[i])
+            {
+                // count candidates for this ticket field, storing the most recent
+                int candidate_count = 0;
+                int last_candidate = -1;
+                for(int j = 0; j < num_fields && candidate_count < 2; ++j)
+                {
+                    if(is_candidate[j][i])
+                    {
+                        candidate_count += 1;
+                        last_candidate = j;
+                    }
+                }
+
+                // only one option
+                if(candidate_count == 1)
+                {
+                    field_assigned[last_candidate] = i;
+                    field_used[i] = 1;
+                    for(int j = 0; j < num_fields; ++j)
+                    {
+                        // remove this field from consideration from other ticket fields
+                        is_candidate[last_candidate][j] = 0;
+                    }
+                    remaining_candidates -= 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    unsigned long product = 1;
+
+    for(int i = 0; i < num_fields; ++i)
+    {
+        char departure_str[10] = "departure";
+        if(strlen(field_names[i]) > strlen(departure_str))
+        {
+            char test[10];
+            memcpy(test, field_names[i], strlen(departure_str));
+            test[9] = '\0';
+            if(!strcmp(departure_str, test))
+            {
+                product *= (unsigned long) tickets[0][field_assigned[i]];
+            }
+        }
+    }
+
+    return product;
 }
 
 int main(int argc, char *argv[])
@@ -196,7 +321,7 @@ int main(int argc, char *argv[])
     }
 
     printf("Part 1: %d\n", part1(argv[1]));
-    printf("Part 2: %d\n", part2(argv[1]));
+    printf("Part 2: %lu\n", part2(argv[1]));
 
     return 0;
 }
