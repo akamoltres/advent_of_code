b3f22ffb36837f122a67c47481b72fc157afdab5
Abhi K <akamoltres@gmail.com>
2020-12-21 00:27:24 -0800

    2020/21 complete

--- 2020/21.c
+++ 2020/21.c
@@ -6,6 +6,7 @@
 #define MAX_NUM_FOODS 100
 #define MAX_NUM_ITEM 500
 #define MAX_ITEM_LEN 15
+#define MAX_RETVAL_LEN 100
 
 typedef struct Food
 {
@@ -71,11 +72,11 @@ int add_item_if_missing(char list[MAX_NUM_ITEM][MAX_ITEM_LEN], int *current_coun
     return (*current_count);
 }
 
-int get_ingr_idx(char ingrs[MAX_NUM_ITEM][MAX_ITEM_LEN], int num_ingrs, char *ingr)
+int get_item_idx(char items[MAX_NUM_ITEM][MAX_ITEM_LEN], int num_items, char *item)
 {
-    for(int i = 0; i < num_ingrs; ++i)
+    for(int i = 0; i < num_items; ++i)
     {
-        if(!strcmp(ingrs[i], ingr))
+        if(!strcmp(items[i], item))
         {
             return i;
         }
@@ -157,7 +158,7 @@ int part1(char *input_filename)
                             first = 0;
                             for(int l = 0; l < foods[j].num_ingr; ++l)
                             {
-                                int ingr_idx = get_ingr_idx(ingrs, num_ingrs, foods[j].ingr[l]);
+                                int ingr_idx = get_item_idx(ingrs, num_ingrs, foods[j].ingr[l]);
                                 if(!ingr_is_matched(aller_src, num_allers, ingr_idx))
                                 {
                                     candidates[ingr_idx] = 1;
@@ -169,7 +170,7 @@ int part1(char *input_filename)
                         {
                             for(int l = 0; l < foods[j].num_ingr; ++l)
                             {
-                                int ingr_idx = get_ingr_idx(ingrs, num_ingrs, foods[j].ingr[l]);
+                                int ingr_idx = get_item_idx(ingrs, num_ingrs, foods[j].ingr[l]);
                                 if(candidates[ingr_idx])
                                 {
                                     candidates[ingr_idx] = 2;
@@ -220,16 +221,163 @@ int part1(char *input_filename)
     {
         for(int j = 0; j < foods[i].num_ingr; ++j)
         {
-            hypo_count += !ingr_is_matched(aller_src, num_allers, get_ingr_idx(ingrs, num_ingrs, foods[i].ingr[j]));
+            hypo_count += !ingr_is_matched(aller_src, num_allers, get_item_idx(ingrs, num_ingrs, foods[i].ingr[j]));
         }
     }
 
     return hypo_count;
 }
 
-int part2(char *input_filename)
+void part2(char *input_filename, char retval[MAX_RETVAL_LEN])
 {
-    return -1;
+    // init
+    int num_foods = 0;
+    Food_t foods[MAX_NUM_FOODS];
+
+    int num_ingrs = 0;
+    char ingrs[MAX_NUM_ITEM][MAX_ITEM_LEN];
+    memset(ingrs, 0, MAX_NUM_ITEM * MAX_ITEM_LEN * sizeof(char));
+
+    int num_allers = 0;
+    char allers[MAX_NUM_ITEM][MAX_ITEM_LEN];
+    memset(allers, 0, MAX_NUM_ITEM * MAX_ITEM_LEN * sizeof(char));
+
+    int aller_src[MAX_NUM_ITEM];
+    for(int i = 0; i < MAX_NUM_ITEM; ++i)
+    {
+        aller_src[i] = -1;
+    }
+
+    // input
+    num_foods = get_input(input_filename, foods);
+
+    // build lists of ingredients and allergens
+    for(int i = 0; i < num_foods; ++i)
+    {
+        for(int j = 0; j < foods[i].num_ingr; ++j)
+        {
+            assert(add_item_if_missing(ingrs, &num_ingrs, foods[i].ingr[j]) < MAX_NUM_ITEM);
+        }
+
+        for(int j = 0; j < foods[i].num_aller; ++j)
+        {
+            assert(add_item_if_missing(allers, &num_allers, foods[i].aller[j]) < MAX_NUM_ITEM);
+        }
+    }
+
+    // match each allergen to an ingredient
+    for(int try = 0; try < num_allers; ++try)
+    {
+        for(int i = 0; i < num_allers; ++i)
+        {
+            int candidates[MAX_NUM_ITEM];
+            memset(candidates, 0, MAX_NUM_ITEM * sizeof(int));
+
+            int first = 1;
+
+            // walk through the foods
+            for(int j = 0; j < num_foods; ++j)
+            {
+                // walk through the allergens in the food
+                for(int k = 0; k < foods[j].num_aller; ++k)
+                {
+                    // if the allergen is the one we are currently looking at
+                    if(!strcmp(allers[i], foods[j].aller[k]))
+                    {
+                        // if it is the first instance, we have our initial list of candidates
+                        if(first)
+                        {
+                            first = 0;
+                            for(int l = 0; l < foods[j].num_ingr; ++l)
+                            {
+                                int ingr_idx = get_item_idx(ingrs, num_ingrs, foods[j].ingr[l]);
+                                if(!ingr_is_matched(aller_src, num_allers, ingr_idx))
+                                {
+                                    candidates[ingr_idx] = 1;
+                                }
+                            }
+                        }
+                        // if it is not the first instance, we can whittle down the list of candidates
+                        else
+                        {
+                            for(int l = 0; l < foods[j].num_ingr; ++l)
+                            {
+                                int ingr_idx = get_item_idx(ingrs, num_ingrs, foods[j].ingr[l]);
+                                if(candidates[ingr_idx])
+                                {
+                                    candidates[ingr_idx] = 2;
+                                }
+                            }
+                            for(int l = 0; l < num_ingrs; ++l)
+                            {
+                                if(candidates[l] == 2)
+                                {
+                                    candidates[l] = 1;
+                                }
+                                else if(candidates[l] == 1)
+                                {
+                                    candidates[l] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // there should be one candidate remaining
+            int candidate = -1;
+            for(int j = 0; j < num_ingrs; ++j)
+            {
+                if(candidates[j] == 1)
+                {
+                    if(candidate == -1)
+                    {
+                        candidate = j;
+                    }
+                    else
+                    {
+                        candidate = -2;
+                    }
+                }
+            }
+            if(candidate >= 0)
+            {
+                aller_src[i] = candidate;
+            }
+        }
+    }
+
+    // bubble sort time
+    char allers_sorted[MAX_NUM_ITEM][MAX_ITEM_LEN];
+    memcpy(allers_sorted, allers, MAX_NUM_ITEM * MAX_ITEM_LEN * sizeof(char));
+    for(int i = 0; i < num_allers; ++i)
+    {
+        for(int j = 1; j < num_allers; ++j)
+        {
+            if(strcmp(allers_sorted[j - 1], allers_sorted[j]) > 0)
+            {
+                char tmp[MAX_ITEM_LEN];
+                strcpy(tmp, allers_sorted[j - 1]);
+                strcpy(allers_sorted[j - 1], allers_sorted[j]);
+                strcpy(allers_sorted[j], tmp);
+            }
+        }
+    }
+
+    // return the sorted results
+    memset(retval, 0, MAX_RETVAL_LEN * sizeof(char));
+    int offset = 0;
+    for(int i = 0; i < num_allers; ++i)
+    {
+        char *ingr = ingrs[aller_src[get_item_idx(allers, num_allers, allers_sorted[i])]];
+        assert(strlen(ingr) + 1 + offset < MAX_RETVAL_LEN);
+        strcpy(retval + offset, ingr);
+        offset += strlen(ingr);
+        if(i != num_allers - 1)
+        {
+            retval[offset++] = ',';
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -241,7 +389,10 @@ int main(int argc, char *argv[])
     }
 
     printf("Part 1: %d\n", part1(argv[1]));
-    printf("Part 2: %d\n", part2(argv[1]));
+
+    char canonical_list[MAX_RETVAL_LEN];
+    part2(argv[1], canonical_list);
+    printf("Part 2: %s\n", canonical_list);
 
     return 0;
 }
