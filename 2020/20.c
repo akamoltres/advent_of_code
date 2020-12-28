fb93e5759bbd04efd808528f02ac1adc60c42b69
Abhi K <akamoltres@gmail.com>
2020-12-27 23:44:12 -0800

    assembles grid

--- 2020/20.c
+++ 2020/20.c
@@ -21,6 +21,17 @@ typedef struct TileSides
     char s[8][TILE_DIM + 1];
 } TileSides_t;
 
+/*
+static void print_tile(Tile_t t)
+{
+    printf("%lu\n", t.id);
+    for(int i = 0; i < TILE_DIM; ++i)
+    {
+        printf("%s\n", t.p[i]);
+    }
+}
+*/
+
 int get_inputs(char *input_filename, Tile_t tiles[MAX_NUM_TILES])
 {
     FILE *fp = fopen(input_filename, "r");
@@ -154,6 +165,19 @@ void rot90(Tile_t *tile)
     memcpy(tile->p, rot.p, TILE_DIM * (TILE_DIM + 1) * sizeof(char));
 }
 
+void flip(Tile_t *tile)
+{
+    Tile_t flip;
+    memset(flip.p, 0, TILE_DIM * (TILE_DIM + 1) * sizeof(char));
+    
+    for(int i = 0; i < TILE_DIM; ++i)
+    {
+        strcpy_flip(flip.p[i], tile->p[i]);
+    }
+
+    memcpy(tile->p, flip.p, TILE_DIM * (TILE_DIM + 1) * sizeof(char));
+}
+
 // side 1 is top
 // side 2 is right
 // side 3 is bottom
@@ -171,6 +195,7 @@ void get_side(Tile_t *tile, int side, char s[TILE_DIM + 1])
                 s[i] = tile->p[i][TILE_DIM - 1];
             }
             s[TILE_DIM] = '\0';
+            break;
         case 3:
             strcpy(s, tile->p[TILE_DIM - 1]);
             break;
@@ -186,17 +211,6 @@ void get_side(Tile_t *tile, int side, char s[TILE_DIM + 1])
     }
 }
 
-/*
-static void print_tile(Tile_t t)
-{
-    printf("%lu\n", t.id);
-    for(int i = 0; i < TILE_DIM; ++i)
-    {
-        printf("%s\n", t.p[i]);
-    }
-}
-*/
-
 // side 1 is top
 // side 2 is right
 // side 3 is bottom
@@ -210,20 +224,46 @@ int compare_sides(Tile_t *tile1, int side1, Tile_t *tile2, int side2)
     get_side(tile1, side1, t1s);
     get_side(tile2, side2, t2s);
 
-    printf("%s %s\n", t1s, t2s);
-
     return strcmp(t1s, t2s);
 }
 
 int side_has_match(int tile_idx, int side, int num_tiles, Tile_t tiles[MAX_NUM_TILES])
 {
+    char candidate_side[TILE_DIM + 1];
+    get_side(&tiles[tile_idx], side, candidate_side);
+
+    for(int i = 0; i < num_tiles; ++i)
+    {
+        if(i != tile_idx)
+        {
+            for(int j = 1; j <= 4; ++j)
+            {
+                char test_side[TILE_DIM + 1];
+
+                get_side(&tiles[i], j, test_side);
+                if(!strcmp(candidate_side, test_side))
+                {
+                    return 1;
+                }
+
+                char test_side_flipped[TILE_DIM + 1];
+                strcpy_flip(test_side_flipped, test_side);
+                if(!strcmp(candidate_side, test_side_flipped))
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    return 0;
 }
 
 void assemble_grid(int num_tiles, int side_length,
                    Tile_t tiles[MAX_NUM_TILES],
                    Tile_t grid[MAX_SIDE_LEN][MAX_SIDE_LEN])
 {
-    // int tile_used[MAX_NUM_TILES] = {0};
+    int tile_used[MAX_NUM_TILES] = {0};
 
     // step 0: generate strings for all the edges
     TileSides_t sides[MAX_NUM_TILES];
@@ -240,11 +280,97 @@ void assemble_grid(int num_tiles, int side_length,
         }
     }
     assert(first_corner != -1);
-    printf("%lu\n", tiles[first_corner].id);
 
-    // step 2: place the corner tile
+    // step 2: orient and place the corner tile
+    int first_corner_oriented = 0;
+    for(int i = 0; i < 4; ++i)
+    {
+        if(side_has_match(first_corner, 1, num_tiles, tiles) || side_has_match(first_corner, 4, num_tiles, tiles))
+        {
+            rot90(&tiles[first_corner]);
+        }
+        else
+        {
+            first_corner_oriented = 1;
+            assert(!side_has_match(first_corner, 1, num_tiles, tiles));
+            assert(!side_has_match(first_corner, 4, num_tiles, tiles));
+            assert(side_has_match(first_corner, 2, num_tiles, tiles));
+            assert(side_has_match(first_corner, 3, num_tiles, tiles));
+            break;
+        }
+    }
+    assert(first_corner_oriented);
+    tile_used[first_corner] = 1;
+    memcpy(&grid[0][0], &tiles[first_corner], sizeof(Tile_t));
 
     // step 3: walk through the grid, placing tiles as they are found to fit
+    for(int r = 0; r < side_length; ++r)
+    {
+        for(int c = 0; c < side_length; ++c)
+        {
+            // first corner is already placed
+            if(r == 0 && c == 0)
+            {
+                continue;
+            }
+
+            // try all orientations of all tiles until one fits
+            int tile_placed = 0;
+            for(int i = 0; !tile_placed && i < num_tiles; ++i)
+            {
+                if(!tile_used[i])
+                {
+                    for(int j = 0; !tile_placed && j < 2; ++j)
+                    {
+                        for(int k = 0; !tile_placed && k < 4; ++k)
+                        {
+                            int top_good = (r == 0);
+                            int left_good = (c == 0);
+
+                            if(!top_good)
+                            {
+                                char top[TILE_DIM + 1] = {0};
+                                get_side(&tiles[i], 1, top);
+
+                                char bottom[TILE_DIM + 1] = {0};
+                                get_side(&grid[r - 1][c], 3, bottom);
+                                top_good = (strcmp(top, bottom) == 0);
+                            }
+
+                            if(!left_good)
+                            {
+                                char left[TILE_DIM + 1] = {0};
+                                get_side(&tiles[i], 4, left);
+
+                                char right[TILE_DIM + 1] = {0};
+                                get_side(&grid[r][c - 1], 2, right);
+
+                                left_good = (strcmp(left, right) == 0);
+                            }
+
+                            tile_placed = (top_good && left_good);
+
+                            if(!tile_placed)
+                            {
+                                rot90(&tiles[i]);
+                            }
+                            else
+                            {
+                                tile_used[i] = 1;
+                                memcpy(&grid[r][c], &tiles[i], sizeof(Tile_t));
+                            }
+                        }
+                        
+                        if(!tile_placed)
+                        {
+                            flip(&tiles[i]);
+                        }
+                    }
+                }
+            }
+            assert(tile_placed);
+        }
+    }
 }
 
 int part2(char *input_filename)
