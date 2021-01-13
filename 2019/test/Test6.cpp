74085e1de804e4f6a16fd37aa08515b0d72d836f
Abhi K <akamoltres@gmail.com>
2021-01-13 00:44:26 -0800

    Completed 2019/6 unit tests

--- 2019/test/Test6.cpp
+++ 2019/test/Test6.cpp
@@ -10,4 +10,14 @@ TEST(DAY_6, TEST_SOL)
 {
     CHECK_EQUAL(154386, solve_2019_6_1("input/6.txt"));
     CHECK_EQUAL(346, solve_2019_6_2("input/6.txt"));
-};
+}
+
+TEST(DAY_6, TEST_0)
+{
+    CHECK_EQUAL(42, solve_2019_6_1("input/6_test0.txt"));
+}
+
+TEST(DAY_6, TEST_1)
+{
+    CHECK_EQUAL(4, solve_2019_6_2("input/6_test1.txt"));
+}
