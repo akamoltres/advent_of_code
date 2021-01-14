191653622ba7f7ad7a79c1b4b8a1d0a6b235c616
Abhi K <akamoltres@gmail.com>
2021-01-14 00:53:34 -0800

    2019/7 complete

--- 2019/test/Test7.cpp
+++ 2019/test/Test7.cpp
@@ -9,7 +9,7 @@ TEST_GROUP(DAY_7)
 TEST(DAY_7, TEST_SOL)
 {
     CHECK_EQUAL(46014, solve_2019_7_1("input/7.txt"));
-    // CHECK_EQUAL(346, solve_2019_7_2("input/7.txt"));
+    CHECK_EQUAL(19581200, solve_2019_7_2("input/7.txt"));
 }
 
 TEST(DAY_7, TEST_0)
@@ -25,4 +25,14 @@ TEST(DAY_7, TEST_1)
 TEST(DAY_7, TEST_2)
 {
     CHECK_EQUAL(65210, solve_2019_7_1("input/7_test2.txt"));
+}
+
+TEST(DAY_7, TEST_3)
+{
+    CHECK_EQUAL(139629729, solve_2019_7_2("input/7_test3.txt"));
+}
+
+TEST(DAY_7, TEST_4)
+{
+    CHECK_EQUAL(18216, solve_2019_7_2("input/7_test4.txt"));
 }
